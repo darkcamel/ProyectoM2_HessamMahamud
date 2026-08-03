1246e689aceebdbbe8c0f1774b49b23451306db8
darkcamel <h_mahamud_b@hotmail.com>
2026-08-03 16:25:19 -0500

    chore: add testConnection to index.js root

--- index.js
+++ index.js
@@ -16,9 +16,31 @@ app.listen(process.env.PORT, () => {
 app.use(express.json());
 app.use(router);
 
-await pool.query('SELECT NOW()')
+/* await pool.query('SELECT NOW()')
     .then(() => console.log('PostgreSQL conectado correctamente'))
     .catch((error) => {
         console.error('Error conectado a PostgresSQL:', error.message);
         process.exit(1);
-    });
+    }); */
+
+async function testConnection() {
+    try {
+        const result = await pool.query('SELECT NOW() AS hora_actual');
+        console.log('Conexión exitosa a PostgreSQL');
+        console.log('Hora del servidor:', result.rows[0].hora_actual);
+
+        const tables = await pool.query(`
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+        `);
+        console.log('Tablas encontradas:', tables.rows.map(r => r.table_name));
+        await pool.end();
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        process.exit(1);
+    };
+}
+
+testConnection();
