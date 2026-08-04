45a5a4c8ca76f33688b2cdc4d6fb307639187895
darkcamel <h_mahamud_b@hotmail.com>
2026-08-04 16:42:17 -0500

    chore: finish script para ejecutar setup.sql

---
+++ src/config/run-setup-sql.js
@@ -0,0 +1,13 @@
+import { readFileSync } from 'node:fs';
+import { pool } from './pool.config.js';
+
+const sql = readFileSync(new URL('./setup.sql', import.meta.url), 'utf-8');
+
+try {
+    await pool.query(sql);
+    console.log('Tablas creadas correctamente');
+} catch (error) {
+    console.error('Error ejectuando setup.sql', error.messager);
+} finally {
+    await pool.end();
+}
