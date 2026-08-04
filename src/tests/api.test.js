ea1d2e5d677fb1d1eb0fe5c9d5f6d29563e4db8d
darkcamel <h_mahamud_b@hotmail.com>
2026-08-04 11:18:11 -0500

    chore: install vitest

---
+++ src/tests/api.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe('describe el titulo lo que quiero testear', () => {
+    it('descripcion del test unitario', () => {
+        expect(true).toBe(true);
+    })
+});
