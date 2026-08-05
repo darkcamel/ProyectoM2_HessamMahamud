57b2f8b6db1eec7904c8672329784dd088cfbe08
darkcamel <h_mahamud_b@hotmail.com>
2026-08-05 09:41:05 -0500

    chore: add loadEnvFile solo en production

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //ESTE MODULO SE ENCARGA SOLO DE LEVANTAR EL SERVIDOR
 
 import { app } from './src/server.js';
-/* import loadEnvFile from 'node:process';
-process.loadEnvFile('.env'); */ // DONDE VA?
+import { loadEnvFile } from 'node:process';
+process.loadEnvFile('.env'); // DONDE VA?
+
+if (process.env.NODE_ENV !== 'production') {
+    loadEnvFile('.env');
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listen on port ${process.env.PORT}`);
 });
-
-/* if (process.env.NODE_ENV !== 'production') {
-    loadEnvFile('.env');
-} */ //DONDE VA?
