48396ce7f0736ad0bf869ede367ec6c040047ad7
darkcamel <h_mahamud_b@hotmail.com>
2026-08-19 08:27:56 -0500

    fix: reorder import statement in index.js for consistency

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //ESTE MODULO SE ENCARGA SOLO DE LEVANTAR EL SERVIDOR
-
-import { app } from './src/server.js';
-
 if (process.env.NODE_ENV !== 'production') {
     process.loadEnvFile('.env');
 }
+
+import { app } from './src/server.js';
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listen on port ${process.env.PORT}`);
